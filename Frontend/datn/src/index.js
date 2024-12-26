c218d46e317626250feb3dcf2eaa6097579d2d03
tungdvph46171 <tungdvph46171@fpt.edu.vn>
2024-12-26 15:02:19 +0700

    vide2_cauhinhroute_react-router-dom

--- Frontend/datn/src/index.js
+++ Frontend/datn/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+// có thể sẽ cần sửa dòng bên dưới(đầu video2)"
 import "antd/dist/reset.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
