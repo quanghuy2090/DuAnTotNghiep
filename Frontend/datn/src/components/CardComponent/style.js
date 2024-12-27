42105714c05e8305bdf1cf24bb5e4820ed812bdb
hanhkt.ndm <khuonghanh88.ndm.@gmail.com>
2024-12-28 01:13:01 +0700

    slider+card

---
+++ Frontend/datn/src/components/CardComponent/style.js
@@ -0,0 +1,55 @@
+import { Card } from "antd";
+import styled from "styled-components";
+
+export const WrapperCardStyle = styled(Card)`
+with: 200px;
+& img {
+height: 200px;
+width: 200px;
+
+
+}, position: relative;
+
+`
+// export const WrapperImageStyle = styled.image`
+// top: -1px;
+// left: -1px;
+// border-top-left-radius: 3px;
+// postition: absolute;
+// height: 14px| important;
+// width: 68px| important;
+
+// `
+export const StyleNameProduct = styled.div`
+font-weight: 400;
+font-size: 12px;
+line-height: 16px;
+color:rgb(56,56,61);
+
+`
+
+export const WrapperReportText = styled.div`
+font-size: 11px;
+color:rgb(128, 128, 137);
+display: flex;
+aligin-items: center;
+margin: 6px 0 0px;
+
+`
+
+export const WrapperPriceText = styled.div`
+
+color:rgb(255, 66, 78);
+font-size: 16px;
+font-weight: 500;
+margin: 8px 0;
+
+`
+export const WrapperDiscountText = styled.span`
+
+color:rgb(255, 66, 78);
+font-size: 12px;
+font-weight: 500;
+
+
+`
---
+++ Frontend/datn/src/components/HeaderComponent/HeaderComponent.jsx
@@ -0,0 +1,59 @@
+import { Col } from "antd";
+import React from "react";
+import {
+  WrapperHeader,
+  WrapperHeaderAccount,
+  WrapperTextHeader,
+  WrapperTextHeaderSmall,
+} from "./style";
+import Search from "antd/es/transfer/search";
+import {
+  UserOutlined,
+  CaretDownOutlined,
+  ShoppingCartOutlined,
+} from "@ant-design/icons";
+import ButtonInputSearch from "../ButtonInputSearch/ButtonInputSearch";
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <WrapperHeader >
+        <Col span={6}>
+          <WrapperTextHeader>Shop điện thoại</WrapperTextHeader>
+        </Col>
+
+        <Col span={12}>
+          <ButtonInputSearch
+            size="large"
+            textButton="Tìm kiếm"
+            bordered={false}
+            placeholder="Nhập từ khóa cần tìm..."
+            // onSearch={onSearch}
+          />
+        </Col>
+
+        <Col
+          span={6}
+          style={{ display: "flex", gap: "20px", alignItems: "center" }}
+        >
+          <WrapperHeaderAccount>
+            <UserOutlined style={{ fontSize: "30px" }} />
+            <div>
+              <WrapperTextHeaderSmall>Đăng nhập/Đăng ký</WrapperTextHeaderSmall>
+              <div>
+                <WrapperTextHeaderSmall>Tài khoản</WrapperTextHeaderSmall>
+                <CaretDownOutlined />
+              </div>
+            </div>
+          </WrapperHeaderAccount>
+          <div>
+            <ShoppingCartOutlined style={{ fontSize: "30px", color: "#fff" }} />
+            <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
+          </div>
+        </Col>
+      </WrapperHeader>
+    </div>
+  );
+};
+
+export default HeaderComponent;
