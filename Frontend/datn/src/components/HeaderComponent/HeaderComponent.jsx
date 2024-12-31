4caf3a896c9373be11dc016daeb6aee2f892dc3e
XuanHieuu1508 <hieuxuan204@gmail.com>
2024-12-31 14:00:43 +0700

    Giao dien login-logout

--- Frontend/datn/src/components/HeaderComponent/HeaderComponent.jsx
+++ Frontend/datn/src/components/HeaderComponent/HeaderComponent.jsx
@@ -16,17 +16,24 @@ import ButtonInputSearch from "../ButtonInputSearch/ButtonInputSearch";
 
 const HeaderComponent = () => {
   return (
-    <div>
+    <div
+      style={{
+        width: "100%",
+        background: "rgb(26,148,255)",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
       <WrapperHeader>
-        <Col span={6}>
+        <Col span={5}>
           <WrapperTextHeader>Shop điện thoại</WrapperTextHeader>
         </Col>
 
-        <Col span={12}>
+        <Col span={13}>
           <ButtonInputSearch
             size="large"
-            textButton="Tìm kiếm"
             bordered={false}
+            textButton="Tìm kiếm"
             placeholder="Nhập từ khóa cần tìm..."
             // onSearch={onSearch}
           />
@@ -34,7 +41,7 @@ const HeaderComponent = () => {
 
         <Col
           span={6}
-          style={{ display: "flex", gap: "20px", alignItems: "center" }}
+          style={{ display: "flex", gap: "54px", alignItems: "center" }}
         >
           <WrapperHeaderAccount>
             <UserOutlined style={{ fontSize: "30px" }} />
--- Frontend/datn/src/pages/SignUpPage/SignUpPage.jsx
+++ Frontend/datn/src/pages/SignUpPage/SignUpPage.jsx
@@ -1,7 +1,82 @@
 import React from "react";
+import {
+  WrapperContainerLeft,
+  WrapperContainerRight,
+  WrapperTextLight,
+} from "../SignInPage/style";
+import InputForm from "../../components/InputForm/InputForm";
+import imageLogo from "../../assets/images/logo-login.png";
+import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
+import { Image } from "antd";
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#ccc",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          width: "800px",
+          height: "445px",
+          borderRadius: "6px",
+          background: "#fff",
+          display: "flex",
+        }}
+      >
+        <WrapperContainerLeft>
+          <h1>Xin chào</h1>
+          <p>Đăng nhập và tạo tài khoản</p>
+          <InputForm
+            style={{ marginBottom: "10px" }}
+            placeholder="abc@gmail.com"
+          />
+          <InputForm style={{ marginBottom: "10px" }} placeholder="Password" />
+          <InputForm placeholder=" Confirm Password" />
+          <ButtonComponent
+            bordered={false}
+            size={40}
+            styleButton={{
+              background: "rgb(255, 57, 69)",
+              height: "48px",
+              width: "100%",
+              border: "none",
+              borderRadius: "4px",
+              margin: "26px 0 10px",
+            }}
+            textButton={"Đăng nhập"}
+            styleTextButton={{
+              color: "#fff",
+              fontsize: "15px",
+              fontWeight: "700",
+            }}
+          ></ButtonComponent>
+
+          <p>
+            Bạn đã có tài khoản?
+            <WrapperTextLight>
+              <span>Đăng ký</span>
+            </WrapperTextLight>
+          </p>
+        </WrapperContainerLeft>
+        <WrapperContainerRight>
+          <Image
+            src={imageLogo}
+            preview={false}
+            alt="image-logo"
+            height="203px"
+            width="203px"
+          />
+          <h4>Mua sắm tại Shop điện thoại</h4>
+        </WrapperContainerRight>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
