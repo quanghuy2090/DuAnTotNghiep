e1fa2eac93b6284f4e557c310ed687d3b332a29f
khuongthanh29040 <khuongthanh290404@gmail.com>
2025-01-03 22:25:01 +0700

    Thiết kế database và connect tới mongodb

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config();
 
 const app = express();
@@ -7,6 +8,14 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+mongoose
+  .connect("mongodb://localhost:27017/duantotnghiep2025")
+  .then(() => {
+    console.log("connection db success");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(port, () => {
   console.log("server is running in port", +port);
 });
