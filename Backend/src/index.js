fa9587659142025bc6cf9996094d6fb3ae7f2cc5
TCSon03 <truongcongsson.@gmail.com>
2025-01-04 13:39:32 +0700

    feat: error

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
 dotenv.config();
 
 const app = express();
@@ -9,6 +10,17 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+mongoose
+  .connect(
+    'mongosh "mongodb+srv://duantotnghiep.9ezwm.mongodb.net/" --apiVersion 1 --username duantotnghiep'
+  )
+  .then(() => {
+    console.log("Connect Db success!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log("Server is runing in port: ", +port);
---
+++ Backend/src/models/UserModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, defaultd: false, required: true },
+    phone: { type: Number, required: true },
+    access_token: { type: String, requere: true },
+    refresh_token: { type: String, require: true },
+  },
+  {
+    timeseries: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
