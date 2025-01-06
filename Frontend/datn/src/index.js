12c5936a9c3de8428101d30a9354245d577cf58f
quanghuy2090 <162445792+quanghuy2090@users.noreply.github.com>
2025-01-06 21:00:34 +0700

    feat: Tich hop react-query vao du an

--- Frontend/datn/src/index.js
+++ Frontend/datn/src/index.js
@@ -6,16 +6,20 @@ import App from "./App";
 import "antd/dist/reset.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient()
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
