b9640832972fb606573f714fc04c68d69ab8137f
quanghuy2090 <162445792+quanghuy2090@users.noreply.github.com>
2025-01-06 21:02:24 +0700

    feat: Tich hop react-query vao du an

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,13 +3,17 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
+const cors = require("cors")
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+
 app.get("/", (req, res) => {
   res.send("hello world hello");
 });
+
+app.use(cors());
 app.use(bodyParser.json());
 routes(app);
 
